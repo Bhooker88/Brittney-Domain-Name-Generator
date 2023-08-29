4828a7febe0a94161510c9de8f67358d5f42a93f
Bhooker88 <139884664+Bhooker88@users.noreply.github.com>
2023-08-29 17:12:52 +0000

    first commit domain name

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["The", "Our"];
+  let adj = ["cool", "big", "smart", "important", "super"];
+  let noun = ["code", "geeks", "developers", "tech", "coders", "nerds"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        let div = document.createElement(`div`);
+        div.innerText = `
+        ${pronoun[i]}${adj[a]}${noun[n]}.com
+        ${pronoun[i]}${adj[a]}${noun[n]}.org
+        ${pronoun[i]}${adj[a]}${noun[n]}.net
+        ${pronoun[i]}${adj[a]}${noun[n]}.io
+        `;
+        document.body.appendChild(div);
+      }
+    }
+  }
 };
